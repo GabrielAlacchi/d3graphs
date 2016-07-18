b13643002fe27d5ddb265b9e2dc888d7c10c280a
GabrielAlacchi <alacchi.g@gmail.com>
2016-07-17 21:27:09 -0400

    Began adding line graph functionality

--- public/lib/d3graphs.js
+++ public/lib/d3graphs.js
@@ -2,7 +2,50 @@
  * Created by gabriel on 17/07/16.
  */
 
-//Testing whether d3 imports properly
-data = [1,2,3];
-data.sort(d3.descending);
+d3graphs = {
+    line: function(id, title) {
 
+        var _width = 0, _height = 0;
+        var _x_series = [], _y_series = [];
+        var svg = d3.select('body').append('svg')
+            .attr('class', id);
+
+        this.width = function(width) {
+            svg.attr('width', width);
+            width = _width;
+            return this;
+        };
+
+        this.height = function(height) {
+            svg.attr('height', height);
+            _height = height;
+            return this;
+        };
+
+        this.data = {
+            //Interface for replacing/appending data to the series of the graph
+            x: function(x_series) {
+                _x_series = x_series;
+                return this;
+            },
+            y: function(y_series) {
+                _y_series = y_series;
+                return this;
+            },
+            append: {
+                x: function(x) {
+                    _x_series = _x_series.concat(x);
+                },
+                y: function(y) {
+                    _y_series = _y_series.concat(y);
+                }
+            }
+        };
+
+        this.update = function() {
+        };
+
+        return this;
+
+    }
+};
